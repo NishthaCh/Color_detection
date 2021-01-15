6ea6921986d512e0d0e094b4dbc9a3ed62707ee6
NishthaCh <67361090+NishthaCh@users.noreply.github.com>
2021-01-15 15:53:14 +0530

    Add files via upload

---
+++ detect_red.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+from skimage.color import rgb2hsv
+from matplotlib import pyplot as plt
+
+img=cv2.imread("colorpic.jpg")
+img_hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# lower mask (0-10)
+lower_red = np.array([0,50,50])
+upper_red = np.array([10,255,255])
+mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+# upper mask (170-180)
+lower_red = np.array([170,50,50])
+upper_red = np.array([180,255,255])
+mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+# join my masks
+mask = mask0+mask1
+
+# set my output img to zero everywhere except my mask
+output_img = img.copy()
+output_img[np.where(mask==0)] = 0
+
+# or your HSV image, which I *believe* is what you want
+output_hsv = img_hsv.copy()
+output_hsv[np.where(mask==0)] = 0
+
+cv2.imshow('mask', mask)
+# image displaying only red colour
+cv2.imshow('output_img', output_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
